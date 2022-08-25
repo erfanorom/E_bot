ba3ebc0aced605ac1024a53ba9c8e661f8999a67
erfanorom <102879878+erfanorom@users.noreply.github.com>
2022-08-25 17:57:35 +0430

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,11 +1,15 @@
 const { loadCommands } = require("../../Handlers/commandHandler");
+const { ActivityType } = require('discord.js');
 
 module.exports = {
   name: "ready",
   once: true,
-  execute(client) {
+  async execute(client) {
     console.log("The Client is now ready.");
-
+    client.user.setPresence({
+      activities: [{ name: `${(await client.guilds.fetch('721833153060274218')).memberCount} members`, type: ActivityType.Watching }], 
+      status: 'idle',
+    });
 
    loadCommands(client);
   }
